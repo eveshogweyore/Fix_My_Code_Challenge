af5cff9c20b9cf2c0135ba613b731eed4d1fc0d0
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2024-05-15 23:52:47 +0100

    Logic completely revamped.

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-""" This comment is freaking  me out. """
+""" This comment is freaking me out. """
 
 
 class square():
     """A square class."""
 
-    def __init__(self, *args, **kwargs):
+    width = 0
+    height = 0
+
+    #def __init__(self, *args, **kwargs):
+    def __init__(self, side):
         """ The constructor. """
-        if kwargs:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-        else:
-            self.width = args[0]
-            self.height = args[1]
-        #self.height = self.width
+        #if kwargs:
+            #for key, value in kwargs.items():
+                #setattr(self, key, value)
+        #else:
+            #self.width = args[0]
+            #self.height = args[1]
+        self.height = side
+        self.width = side
 
     def area_of_my_square(self):
         """ Area of the square. """
@@ -29,7 +34,8 @@ class square():
 
 
 if __name__ == "__main__":
-    s = square(width=12, height=9)
+    #s = square(width=12, height=9)
+    s = square(12)
     print(s)
     print(s.area_of_my_square())
     print(s.PerimeterOfMySquare())
