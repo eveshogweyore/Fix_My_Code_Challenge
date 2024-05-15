b8b820fcc42f2ffb9e1379722fc90d4982d3f030
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2024-05-16 00:13:16 +0100

    Pycodestyle errors vamosed..

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
-""" This comment is freaking me out. """
+""" A SQUARE module. """
 
 
-class square():
-    """A square class."""
-
+class Square():
+    """ The Square class. """
     width = 0
     height = 0
 
-    def __init__(self, side):
+    def __init__(self, *args, **kwargs):
         """ The constructor. """
-        self.height = side
-        self.width = side
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square. """
+        """ Area of the square """
         return self.width * self.height
 
-    def PerimeterOfMySquare(self):
-        """ Perimeter of a square. """
-        return (2 * self.width) + (2 * self.height)
+    def perimeter_of_my_square(self):
+        """ Perimeter of the square """
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ String representation of square class. """
+        """ String representation. """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    s = square(12)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PerimeterOfMySquare())
+    print(s.perimeter_of_my_square())
